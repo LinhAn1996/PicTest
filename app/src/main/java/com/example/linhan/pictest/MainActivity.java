4cda3c8f9c568d402653ea94e20dbf4e21fc13f6
LinhAn1996 <34492745+LinhAn1996@users.noreply.github.com>
2019-01-23 14:42:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/linhan/pictest/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.linhan.pictest;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnSend;
+    ImageView imageView;
+    Button btnCamera;
+    Button btnGallery;
+    Button btnRotate;
+    String imageString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnSend = findViewById(R.id.btnSend);
+        btnCamera = findViewById(R.id.btnCamera);
+        btnGallery = findViewById(R.id.btnGallery);
+        btnRotate = findViewById(R.id.btnRotate);
+        imageView = findViewById(R.id.imageView);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(gallery, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+}
